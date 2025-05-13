e22243ac8cd5a1322dcef54c982ee3fbce07c6f6
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2025-05-13 18:53:35 -0400

    feat: [bytecode][02] Added first bits of compiler code

---
+++ compiler/compiler.go
@@ -0,0 +1,30 @@
+package compiler
+
+import (
+	"monkey-lang/code"
+	"monkey-lang/object"
+)
+
+type Compiler struct {
+	instructions code.Instructions
+	constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
