98af41d089945e53672cfd743c1e1af8143c7dde
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2025-05-22 06:44:14 -0400

    chore: [cytecode][02] Hooked-up REPL

--- repl/repl.go
+++ repl/repl.go
@@ -4,20 +4,19 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"monkey-lang/evaluator"
+	"monkey-lang/compiler"
 	"monkey-lang/lexer"
-	"monkey-lang/object"
 	"monkey-lang/parser"
+	"monkey-lang/vm"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-	env := object.NewEnvironment()
 
 	for {
-		fmt.Fprint(out, PROMPT)
+		fmt.Fprintf(out, PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
@@ -33,11 +32,24 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program, env)
-		if evaluated != nil {
-			io.WriteString(out, evaluated.Inspect())
-			io.WriteString(out, "\n")
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Compilation failed:\n %s\n", err)
+			continue
 		}
+
+		machine := vm.New(comp.Bytecode())
+		err = machine.Run()
+
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Executing bytecode failed:\n %s\n", err)
+			continue
+		}
+
+		stackTop := machine.StackTop()
+		io.WriteString(out, stackTop.Inspect())
+		io.WriteString(out, "\n")
 	}
 }
 
