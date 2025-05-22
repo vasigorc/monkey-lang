53abee122e66fbeb59dc6308002d679c884ca301
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2025-05-22 00:00:54 -0400

    chore: [bytecode][02] Added addition to compiler

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1,6 +1,7 @@
 package compiler
 
 import (
+	"fmt"
 	"monkey-lang/ast"
 	"monkey-lang/code"
 	"monkey-lang/object"
@@ -42,6 +43,13 @@ func (c *Compiler) Compile(node ast.Node) error {
 		err = c.Compile(node.Right)
 		if err != nil {
 			return err
+		}
+
+		switch node.Operator {
+		case "+":
+			c.emit(code.OpAdd)
+		default:
+			return fmt.Errorf("unknown ")
 		}
 	case *ast.IntegerLiteral:
 		integer := &object.Integer{Value: node.Value}
