4c7102cce3ba490601a9f17434e9cae760dcf02d
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2025-05-24 18:32:16 -0400

    feat: [expressions][03] Added OpPop

--- vm/vm.go
+++ vm/vm.go
@@ -54,6 +54,8 @@ func (vm *VM) Run() error {
 			rightValue := right.(*object.Integer).Value
 			result := leftValue + rightValue
 			vm.push(&object.Integer{Value: result})
+		case code.OpPop:
+			vm.pop()
 		}
 	}
 
@@ -75,4 +77,8 @@ func (vm *VM) pop() object.Object {
 	o := vm.stack[vm.sp-1]
 	vm.sp--
 	return o
+}
+
+func (vm *VM) LastPoppedStackElem() object.Object {
+	return vm.stack[vm.sp]
 }
