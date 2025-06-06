81cc720d203ee2b79001361de9c55cb0b9c97ff4
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2025-06-06 00:47:03 -0400

    feat: [variables][05] Added Symbol Table

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -153,6 +153,11 @@ func (c *Compiler) Compile(node ast.Node) error {
 		} else {
 			c.emit(code.OpFalse)
 		}
+	case *ast.LetStatement:
+		err := c.Compile(node.Value)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
