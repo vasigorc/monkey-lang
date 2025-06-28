ee685064476e5252b8bd2fbe400c7974ceb04c0b
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2025-06-28 15:04:03 -0400

    feat: [builtins][08] Moved len builtin to object package

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -6,38 +6,8 @@ import (
 )
 
 var builtins = map[string]*object.Builtin{
-	"len": {Fn: func(args ...object.Object) object.Object {
-		if len(args) != 1 {
-			return newError("wrong number of arguments. got=%d, want=1", len(args))
-		}
-
-		switch arg := args[0].(type) {
-		case *object.Array:
-			return &object.Integer{Value: int64(len(arg.Elements))}
-		case *object.String:
-			return &object.Integer{Value: int64(len(arg.Value))}
-		default:
-			return newError("argument to `len` not supported, got %s", args[0].Type())
-		}
-	}},
-
-	"first": {
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got=%d, want=1", len(args))
-			}
-			if args[0].Type() != object.ARRAY_OBJ {
-				return newError("argument to `first` must be ARRAY, got %s", args[0].Type())
-			}
-
-			arr := args[0].(*object.Array)
-			if len(arr.Elements) > 0 {
-				return arr.Elements[0]
-			}
-
-			return NULL
-		},
-	},
+	"len":   object.GetBuiltinByName("len"),
+	"first": object.GetBuiltinByName("first"),
 
 	"last": {
 		Fn: func(args ...object.Object) object.Object {
