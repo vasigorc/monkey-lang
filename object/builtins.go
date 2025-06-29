796b300d66307b7af8caa6d913a295560e1769e8
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2025-06-29 17:47:29 -0400

    chore: [builtins][07] Added builtins to the VM

--- object/builtins.go
+++ object/builtins.go
@@ -80,7 +80,7 @@ var Builtins = []struct {
 					return newError("wrong number of arguments. got=%d, want=1", len(args))
 				}
 				if args[0].Type() != ARRAY_OBJ {
-					return newError("argument to `last` must be ARRAY, got %s", args[0].Type())
+					return newError("argument to `rest` must be ARRAY, got %s", args[0].Type())
 				}
 				arr := args[0].(*Array)
 				length := len(arr.Elements)
@@ -97,11 +97,11 @@ var Builtins = []struct {
 	{
 		"push", &Builtin{
 			Fn: func(args ...Object) Object {
-				if len(args) != 1 {
-					return newError("wrong number of arguments. got=%d, want=1", len(args))
+				if len(args) != 2 {
+					return newError("wrong number of arguments. got=%d, want=2", len(args))
 				}
 				if args[0].Type() != ARRAY_OBJ {
-					return newError("argument to `last` must be ARRAY, got %s", args[0].Type())
+					return newError("argument to `push` must be ARRAY, got %s", args[0].Type())
 				}
 				arr := args[0].(*Array)
 				length := len(arr.Elements)
--- vm/vm.go
+++ vm/vm.go
@@ -170,7 +170,7 @@ func (vm *VM) Run() error {
 			numArgs := code.ReadUint8(ins[ip+1:])
 			vm.currentFrame().ip += 1
 
-			err := vm.callFunction(int(numArgs))
+			err := vm.executeCall(int(numArgs))
 			if err != nil {
 				return err
 			}
@@ -209,18 +209,23 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+		case code.OpGetBuiltin:
+			builtinIndex := code.ReadUint8(ins[ip+1:])
+			vm.currentFrame().ip += 1
+
+			definition := object.Builtins[builtinIndex]
+
+			err := vm.push(definition.Builtin)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
 	return nil
 }
 
-func (vm *VM) callFunction(numArgs int) error {
-	fn, ok := vm.stack[vm.sp-1-numArgs].(*object.CompiledFunction)
-	if !ok {
-		return fmt.Errorf("calling non-function")
-	}
-
+func (vm *VM) callFunction(fn *object.CompiledFunction, numArgs int) error {
 	if numArgs != fn.NumParameters {
 		return fmt.Errorf("wrong number of arguments: want=%d, got=%d", fn.NumParameters, numArgs)
 	}
@@ -229,6 +234,21 @@ func (vm *VM) callFunction(numArgs int) error {
 	vm.pushFrame(frame)
 
 	vm.sp = frame.basePointer + fn.NumLocals
+
+	return nil
+}
+
+func (vm *VM) callBuiltin(builtin *object.Builtin, numArgs int) error {
+	args := vm.stack[vm.sp-numArgs : vm.sp]
+
+	result := builtin.Fn(args...)
+	vm.sp = vm.sp - numArgs - 1
+
+	if result != nil {
+		vm.push(result)
+	} else {
+		vm.push(Null)
+	}
 
 	return nil
 }
@@ -467,6 +487,18 @@ func (vm *VM) pushFrame(f *Frame) {
 func (vm *VM) popFrame() *Frame {
 	vm.framesIndex--
 	return vm.frames[vm.framesIndex]
+}
+
+func (vm *VM) executeCall(numArgs int) error {
+	callee := vm.stack[vm.sp-1-numArgs]
+	switch callee := callee.(type) {
+	case *object.CompiledFunction:
+		return vm.callFunction(callee, numArgs)
+	case *object.Builtin:
+		return vm.callBuiltin(callee, numArgs)
+	default:
+		return fmt.Errorf("calling non-function and non-built-in")
+	}
 }
 
 var True = &object.Boolean{Value: true}
